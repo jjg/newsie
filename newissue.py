89e45a68bf57aa1d1bf713dc59147cac49c75a24
requiem <mr@jasongullickson.com>
2020-01-14 09:44:34 -0600

    Initial commit.

---
+++ newissue.py
@@ -0,0 +1,9 @@
+# TODO: Load a list of RSS feeds
+# TODO: Fetch articles from the last 24 hours
+# TODO: Randomly select enough to fill 2 pages
+# TODO: Create new PDF
+# TODO: Render each article into PDF
+# TODO: Insert headline
+# TODO: Insert summary
+# TODO: Insert QR code link to source
+# TODO: Send to printer
