eec3db92ca79a4f4a8637bb9d84a504fab1002fd
Jason J. Gullickson <mr@jasongullickson.com>
2020-04-01 12:18:33 -0500

    Multiple feed support.

--- newissue.py
+++ newissue.py
@@ -13,47 +13,54 @@ from config import config
 # TODO: Load subscriber from some datasource
 subscriber_printer_email = config["debug_email"]
 subscriber_blacklist = ""
-rssfeed = "https://feeds.bbci.co.uk/news/rss.xml"
+subscriber_feeds = [
+        "https://feeds.bbci.co.uk/news/rss.xml",
+        "https://hackaday.com/feed",
+        ]
 
-# Load RSS feed
-# TODO: Load multiple feeds
-feed = feedparser.parse(rssfeed)
-
-# TODO: Filter articles more than 24 hours old (entry["published"])
-# TODO: Filter through subscriber blacklist
-# TODO: Randomly select enough to fill 2 pages
 
 # Render each article into html
+# TODO: Apply style to html
 newspaper_html = "<h1>The Daily Facto</h1>"
 
-entry_idx = 0
-for entry in feed.entries:
+for feed_url in subscriber_feeds:
 
-    # Insert headline
-    newspaper_html += "<hr>"
-    newspaper_html += f"<h2>{entry['title']}</h2>"
-    
-    # Insert summary
-    newspaper_html += f"<p>{entry['summary']}</p>"
+    # Load RSS feed
+    feed = feedparser.parse(feed_url)
 
-    # TODO: Consider grabbing more of the article if the summary is short.
+    # TODO: Filter articles more than 24 hours old (entry["published"])
+    # TODO: Filter through subscriber blacklist
+    # TODO: Strip html, hyperlinks, etc.
+    # TODO: Randomly select enough to fill 2 pages
 
-    # Insert QR code link to source
-    qr_link = qrcode.QRCode(
-            version = 10,
-            box_size = 1,
-            border = 4,
-            )
-    qr_link.add_data(entry["link"])
-    qr_link.make(fit=False)
-    qr_link_img = qr_link.make_image(fill_color="black",back_color="white")
-    qr_link_img.save(f"./img/{entry_idx}.jpg", "JPEG")
+    entry_idx = 0
+    for entry in feed.entries:
 
-    # TODO: Full path required here, but find a way to 
-    # avoid hard-coding it.
-    newspaper_html += f"<img src='/home/jason/Development/paperboy/img/{entry_idx}.jpg'>"
+        # Insert headline
+        newspaper_html += "<hr>"
+        newspaper_html += f"<h2>{entry['title']}</h2>"
+        
+        # Insert summary
+        newspaper_html += f"<p>{entry['summary']}</p>"
 
-    entry_idx = entry_idx + 1
+        # TODO: Consider grabbing more of the article if the summary is short.
+
+        # Insert QR code link to source
+        qr_link = qrcode.QRCode(
+                version = 10,
+                box_size = 1,
+                border = 4,
+                )
+        qr_link.add_data(entry["link"])
+        qr_link.make(fit=False)
+        qr_link_img = qr_link.make_image(fill_color="black",back_color="white")
+        qr_link_img.save(f"./img/{entry_idx}.jpg", "JPEG")
+
+        # TODO: Full path required here, but find a way to 
+        # avoid hard-coding it.
+        newspaper_html += f"<img src='/home/jason/Development/paperboy/img/{entry_idx}.jpg'>"
+
+        entry_idx = entry_idx + 1
 
 # Create new PDF
 newspaper_pdf = pydf.generate_pdf(newspaper_html)
